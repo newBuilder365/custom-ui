ccebfdbdbac214c22b8f41deb519c39767f70194
newbuilder365 <772512900@qq.com>
2023-04-17 09:54:47 +0800

    Updates

---
+++ Select/index.js
@@ -0,0 +1,6 @@
+import { Select } from 'antd';
+import React from 'react';
+var CSelect = function CSelect(props) {
+  return /*#__PURE__*/React.createElement(Select, props);
+};
+export default CSelect;
